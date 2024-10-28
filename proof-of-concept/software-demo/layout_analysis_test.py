d8f59cbd3c4530c84df10a12c3906385c91d7f13
Yucheng Yao <alexyyc13002251517@gmail.com>
2024-10-28 16:59:08 -0400

    Updated code and .gitignore with correct image format

--- proof-of-concept/software-demo/layout_analysis_test.py
+++ proof-of-concept/software-demo/layout_analysis_test.py
@@ -30,7 +30,7 @@ model = YOLO("capstone_yolov8_trained.pt")
 
 ocr = PaddleOCR(use_angle_cls=True, lang='en')
 
-source = "target.png"
+source = "target.jpg"
 image = cv2.imread(source)
 
 results = model(image) 
